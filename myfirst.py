a000083817cc6729881797a04a5b38260233656c
AnuPerera1023 <sanuradhaperera@gmail.com>
2025-12-02 20:22:31 +0530

    number guessing game

--- myfirst.py
+++ myfirst.py
@@ -4,5 +4,15 @@ length = 5
 width = 10
 area = length * width
 
+import random
+
+secret_number = 7
+guess = 0
+
+while guess != secret_number:      # keep guessing until it matches
+    guess = random.randint(1, 10)
+    print("Guessing:", guess)
+
+print("I guessed the right number! It was", secret_number)
 
 
